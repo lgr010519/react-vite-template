23292bd92fa17d2e08777681ff94022d65f726d4
zhaosi <zhaosi@yoyohr.com>
2023-10-11 10:53:26 +0800

    feat: 升级了各类包

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { RouterProvider } from 'react-router-dom'
 import router from '@/router'
 function App() {
-  return <RouterProvider router={router}></RouterProvider>
+  return <RouterProvider router={router} />
 }
 
 export default App
