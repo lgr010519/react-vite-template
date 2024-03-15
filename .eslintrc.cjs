1fbbef1390d18a1baad18aa399807d2da1134013
liqianchen <liqianchen@yoyohr.com>
2024-03-15 11:56:23 +0800

    fix: 回滚至3452ee

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,5 @@
 module.exports = {
-  extends: [
-    'react-app',
-    'plugin:import/recommended',
-    'plugin:import/react',
-    'prettier',
-  ],
+  extends: ['react-app', 'prettier'],
   plugins: ['react-refresh'],
   rules: {
     'prefer-const': 2,
@@ -29,36 +24,5 @@ module.exports = {
         html: true, // 不要求对 HTML 标记使用自闭合标签
       },
     ],
-    // 模板字符串的处理``
-    'no-template-curly-in-string': 'off',
-    'import/order': [
-      'warn',
-      {
-        groups: [
-          // https://github.com/import-js/eslint-plugin-import/blob/v2.29.1/docs/rules/order.md
-          'builtin',
-          'external',
-          'internal',
-          'parent',
-          'sibling',
-          'index',
-          'object',
-          'type',
-        ],
-        'newlines-between': 'always', // 可选: 在不同组之间加空行
-      },
-    ],
-    'no-new-func': 'off',
-  },
-  settings: {
-    // 添加别名解析器
-    'import/resolver': {
-      'eslint-import-resolver-custom-alias': {
-        alias: {
-          '@': './src',
-        },
-        extensions: ['.js', '.jsx'],
-      },
-    },
   },
 }
